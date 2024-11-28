9e42f7fc1a92e67f69932fa96d2acff35d131160
Mrityunjay Shukla <155085552+Mrityunjayyshukla@users.noreply.github.com>
2024-11-28 19:23:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Navigate to the project directory
-cd C://Users/shukl/Documents/Codes/PythonBranch/Flask_tut/flutterpython
+cd #Location to your project
 
 # Start the Flask server in the background
 python lib/python/launcher.py &
